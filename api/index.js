3fb53112da53824704adb8255e58affd5d0e5892
aryan24-cs <aryan.nda.2163@gmail.com>
2026-08-23 14:56:05 +0530

    Add error handler in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import app from '../server/index.js';
 
-export default function handler(req, res) {
-    return app(req, res);
+export default async function handler(req, res) {
+    try {
+        return app(req, res);
+    } catch (err) {
+        console.error('Vercel Serverless Handler Error:', err);
+        if (!res.headersSent) {
+            res.status(500).json({ error: 'Serverless Error', message: err.message, stack: err.stack });
+        }
+    }
 }
